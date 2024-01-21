6b99181356ac8aebea10aa2f0c2984ddbe10a63d
Mike .D <txrz56rad@live.com.mx>
2024-01-20 18:13:32 -0600

    post it & new post layout

---
+++ app.js
@@ -0,0 +1,30 @@
+export default class App {
+  static getTasks(columnId) {}
+
+  static insertTask(columnId, content) {}
+
+  static updateTask(taskId, updatedInformation) {}
+
+  static deleteTask(taskId) {}
+
+  static getAllTasks() {
+    const data = read();
+    return [data[0].tasks, data[1].tasks, data[2].tasks];
+  }
+}
+
+const read = () => {
+  const data = localStorage.getItem("data");
+
+  if (!data) {
+    return [
+      { columnId: 0, tasks: [] },
+      { columnId: 1, tasks: [] },
+      { columnId: 2, tasks: [] },
+    ];
+  }
+
+  return JSON.parse(data);
+};
+
+const save = () => {};
---
+++ index.js
@@ -0,0 +1,4 @@
+import App from "./app.js";
+
+console.log("kalem on the track boy");
+console.log(App.getAllTasks());
