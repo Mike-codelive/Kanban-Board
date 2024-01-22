19f8f6a7616fb500997e7b080b03a90ac230b2e3
Mike .D <txrz56rad@live.com.mx>
2024-01-21 23:03:38 -0600

    Logic of App.js

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
 export default class App {
-  static getTasks(columnId) {}
+  static getTasks(columnId) {
+    const data = read().find((column) => {
+      return column.columnId == columnId;
+    });
 
-  static insertTask(columnId, content) {}
+    if (!data) {
+      return [];
+    }
 
-  static updateTask(taskId, updatedInformation) {}
+    return data.tasks;
+  }
 
-  static deleteTask(taskId) {}
+  static insertTask(columnId, content, color) {
+    const data = read();
+    const column = data.find((column) => {
+      return column.columnId == columnId;
+    });
+    if (color === undefined) {
+      color = "green";
+    }
+
+    const task = {
+      taskId: Math.floor(Math.random() * 100000),
+      content: content,
+      color: color,
+      date: getDate(),
+    };
+    console.log(task);
+
+    column.tasks.push(task);
+    save(data);
+    return task;
+  }
+
+  static updateTask(taskId, updatedInformation) {
+    const data = read();
+
+    function findColumnTask() {
+      for (const column of data) {
+        const task = column.tasks.find((item) => {
+          return item.taskId == taskId;
+        });
+
+        if (task) {
+          return [task, column];
+        }
+      }
+    }
+    const [task, currentColumn] = findColumnTask();
+
+    const targetColumn = data.find((column) => {
+      return column.columnId == updatedInformation.columnId;
+    });
+
+    task.content = updatedInformation.content;
+    currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1);
+    targetColumn.tasks.push(task);
+
+    save(data);
+  }
+
+  static deleteTask(taskId) {
+    const data = read();
+
+    for (const column of data) {
+      const task = column.tasks.find((item) => {
+        return item.taskId == taskId;
+      });
+
+      column.tasks.splice(column.tasks.indexOf(task), 1);
+    }
+
+    save(data);
+  }
 
   static getAllTasks() {
     const data = read();
+    columnCount();
     return [data[0].tasks, data[1].tasks, data[2].tasks];
   }
 }
@@ -27,4 +95,30 @@ const read = () => {
   return JSON.parse(data);
 };
 
-const save = () => {};
+const save = (data) => {
+  localStorage.setItem("data", JSON.stringify(data));
+  columnCount();
+};
+
+const getDate = () => {
+  const currentDate = new Date();
+
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const year = currentDate.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+const columnCount = () => {
+  const data = read();
+
+  const todo = document.querySelector("span.todo");
+  todo.textContent = data[0].tasks.length;
+
+  const pending = document.querySelector("span.pending");
+  pending.textContent = data[1].tasks.length;
+
+  const completed = document.querySelector("span.completed");
+  completed.textContent = data[2].tasks.length;
+};
--- index.js
+++ index.js
@@ -1,4 +1,79 @@
 import App from "./app.js";
 
-console.log("kalem on the track boy");
-console.log(App.getAllTasks());
+// console.log("kalem on the track boy");
+// console.log(App.getAllTasks());
+// console.log(App.getTasks());
+// console.log(App.insertTask(0, "test task whit date"));
+// App.deleteTask(43416);
+// console.log(
+//   App.updateTask(67127, {
+//     columnId: 2,
+//     content: "updated task",
+//   })
+// );
+
+const todo = document.querySelector(".cards.todo");
+const pending = document.querySelector(".cards.pending");
+const completed = document.querySelector(".cards.completed");
+const taskbox = [todo, pending, completed];
+
+function addTaskCard(task, index) {
+  const element = document.createElement("form");
+  element.className = "card p-relative";
+  element.draggable = true;
+  element.dataset.id = task.taskId;
+  element.innerHTML = `
+        <div
+        class="post-it post-it-${task.color}"
+        type="text"
+        name="task"
+        autocomplete="off"
+        disabled="disabled"
+      >
+      ${task.content}
+      </div>
+
+      <div>
+      <span class="post-it-date p-absolute">${task.date}</span>
+      <span class="post-it-btn p-absolute">
+        <button class="bi bi-pencil edit" data-id="${task.taskId}">
+          <span class="tooltiptext">Edit</span>
+        </button>
+        <button
+          class="bi bi-check-lg update hide"
+          data-id="${task.taskId}"
+        ></button>
+        <button class="bi bi-color edit-color" data-id="${task.taskId}">
+          <span class="tooltiptext">Change color</span>
+        </button>
+        <button class="bi bi-trash delete" data-id="${task.taskId}">
+          <span class="tooltiptext">Delete</span>
+        </button>
+      </span>
+    </div>
+    `;
+  taskbox[index].appendChild(element);
+}
+
+App.getAllTasks().forEach((tasks, index) => {
+  tasks.forEach((task) => {
+    addTaskCard(task, index);
+  });
+});
+
+const addForm = document.querySelectorAll(".add");
+
+addForm.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (form.task.value) {
+      const task = App.insertTask(
+        form.submit.dataset.id,
+        form.task.value.trim()
+      );
+      console.log(form.colors.value);
+      addTaskCard(task, form.submit.dataset.id);
+      form.reset();
+    }
+  });
+});
